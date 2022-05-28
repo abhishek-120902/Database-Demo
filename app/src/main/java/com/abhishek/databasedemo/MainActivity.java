24ad120dca6dc660d6f9dbc33c7d9e83030e13fa
Abhishek Singh <abhi120902@gmail.com>
2022-05-29 01:10:27 +0530

    Second Commit

--- app/src/main/java/com/abhishek/databasedemo/MainActivity.java
+++ app/src/main/java/com/abhishek/databasedemo/MainActivity.java
@@ -20,19 +20,35 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
             //Create Table in Database if not exists
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INTEGER, id INTEGER PRIMARY KEY AUTOINCREMENT)");
             
             //Insert Data in the table
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Abhishek', 20)");
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Aditya', 18)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Abhishek', 20)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Aditya', 18)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Akash', 22)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Peeyush', 19)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Peeyush', 19)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Shubham', 21)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Karan', 25)");
+            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Shivam', 23)");
+
+            //For deleting duplicate data or a data from table
+            myDatabase.execSQL("DELETE FROM newUsers WHERE id=4");
+            //LIMIT 1 is used to limit the query for only 1 result i.e. Only 1 data will be deleted from the table
+
+            //For updating a value from the result suppose change the age to 24 whose name contains 'K' in his name
+            myDatabase.execSQL("UPDATE newUsers SET age = 24 WHERE name LIKE '%k%'");
 
             //For taking data from the database
             //Cursor allows us to loop through all of the results of a particular query
-            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+            Cursor c = myDatabase.rawQuery("SELECT * FROM newUsers", null);
+
+
             
             //For getting particular column values from table
             int nameIndex = c.getColumnIndex("name");
             int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             //Take out cursor and move to the first result
             c.moveToFirst();
@@ -43,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 //Show the results
                 Log.i("Name", c.getString(nameIndex));
                 Log.i("Age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("Age", Integer.toString(c.getInt(idIndex)));
 
                 //To move to the next result
                 c.moveToNext();
